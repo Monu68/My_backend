de5edb314278e85bd0ff48b418bcefb2e69def85
Manish Kumar Monu <92303219+Monu68@users.noreply.github.com>
2024-07-03 18:17:47 +0530

    commit all

---
+++ src/index.js
@@ -0,0 +1,34 @@
+
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
+
+/*import express from "express";
+const app = express()
+
+//iffe 
+
+( async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) =>{
+            console.log("ERRor",error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is running on port ${process.env.PORT}`);
+        })
+    }catch(error){
+        console.error("Error:", error)
+    }
+
+})()
+*/
